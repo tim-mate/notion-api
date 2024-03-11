56d8b43c68a73bb759da32a9603ee296686d8614
Timur <timour.lutsenko@gmail.com>
2024-03-11 13:47:56 +0200

    feat(pages): add router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import pagesRouter from "modules/pages/pages.router";
+
 const app = express();
 
 const morganFormat = app.get("env") === "development" ? "dev" : "short";
@@ -9,5 +11,11 @@ const morganFormat = app.get("env") === "development" ? "dev" : "short";
 app.use(morgan(morganFormat));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/pages", pagesRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route is not found" });
+});
 
 export default app;
---
+++ src/modules/pages/pages.router.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+import PageController from "./pages.controller";
+
+const pagesRouter = express.Router();
+
+pagesRouter.get("/", PageController.getAll);
+
+pagesRouter.get("/:id", PageController.getOne);
+
+pagesRouter.post("/", PageController.add);
+
+pagesRouter.delete("/:id", PageController.delete);
+
+pagesRouter.patch("/:id/favorite", PageController.updateStatus);
+
+export default pagesRouter;
